1a5b3d6a2621b218cd71eb77028532323118c42f
lock <lohchness@gmail.com>
2022-07-07 18:03:37 +0800

    new commit

---
+++ spotiffline.py
@@ -0,0 +1,17 @@
+import requests
+
+PLAYLIST_ID = "3Zx2NoPvIYWst3GYQrAyb3"
+ACCESS_TOKEN = "BQAT3a4UeCQX036hJw56i3a0xL1bcQKDdm7evy9WI5lR_JSaLIvZzWeetiKOU7zujlwyA0fxpaobVsnOGQjl9fWCRhuY7JFVJ2pd3_ekjjvwnXZcFdx7fKtoRD3d2szGvbJ_auS0GPjXXeBd_EfTeHoT8o916dk8efXvL6b8HohZAcE5g3bN6VN1tx6eVOJ83qSg_yO"
+ENDPOINT_URL = "https://api.spotify.com/v1/playlists/3Zx2NoPvIYWst3GYQrAyb3/tracks"
+
+
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+
