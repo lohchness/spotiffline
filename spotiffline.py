1c8ce3c6b6a6b4b8ab0f33b0f0f21701480db123
lock <lohchness@gmail.com>
2022-07-09 11:11:43 +0800

    Cleaned up, moving to Stage 2
    Removed unncessary deprecated functions like Implicit Grant and Authorization Code Flow authentication functions. Cleaned up constants and imports. Main function is more readable.

--- spotiffline.py
+++ spotiffline.py
@@ -1,35 +1,12 @@
-import requests, base64
-import string, pprint, random # delete when finished
+import requests, base64, json, re
+import youtubesearchpython # CAUTION: LIBRARY NOT MAINTAINED AS OF 23/6/2022
 from secret_ids import CLIENT_ID, CLIENT_SECRET
 
-BASE_URI = "https://api.spotify.com/v1"
-REDIRECT_URI = "https://localhost/"
-PLAYLIST_ID = "3Zx2NoPvIYWst3GYQrAyb3"
-ACCESS_TOKEN = "BQAt1iR6hGGw4h8fTPQqBFkWNgJFTqjiSF2XSv7xVHHcgFNKCVkexu6PMWNCqgeeub-Y17j5noeuxHWFV3SCF_1FIgnWwnasiXVwc9cm92QrwQdhwd3MsDmW1qHtsvBYoI5xW2bXhpi6GZUFYxqO4Hq66sAL3lt1EkgD-M7-rtGoi551qatJf8tves3z8Q0"
-ENDPOINT_PLAYLIST = f"https://api.spotify.com/v1/playlists/{PLAYLIST_ID}/tracks"
+# WILL CHANGE TO ASK USER FOR PLAYLIST LINK
+PLAYLIST_LINK = "https://open.spotify.com/playlist/3Zx2NoPvIYWst3GYQrAyb3?si=1ff2abea630a408d"
+
 FIELDS = """items(added_at,track(name,duration_ms,album(name,images,artists),artists))"""
-ENDPOINT_PLAYLIST_WITH_FIELDS = ENDPOINT_PLAYLIST + f"?fields={FIELDS}"
 ENDPOINT_APITOKEN = "https://accounts.spotify.com/api/token"
-ENDPOINT_AUTHORIZE = "https://accounts.spotify.com/authorize"
-LENGTH = 16
-
-def randomString(length): # DEPRECATED
-    letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for i in range(length))
-
-def auth_implicit_grant(): # DEPRECATED
-    # working, returns code 200 but found out later that this is 
-    # for clients implemented entirely in JS and running in a browser.
-    # URL Hash values are not accessible server side, theyre not included in headers
-    response = requests.get(
-        ENDPOINT_AUTHORIZE,
-        {
-        "client_id" : CLIENT_ID,
-        "response_type" : "token",
-        "redirect_uri" : "spotiffline-login://callback", 
-        "state" : randomString(LENGTH)
-    })
-    return response # returns URL containing hash fragment with data encoded as query string, not JSON object
 
 def auth_client_credentials():
     encodedstr = f"{CLIENT_ID}:{CLIENT_SECRET}".encode()
@@ -40,54 +17,12 @@ def auth_client_credentials():
     # If auth code flow needs external web service we go this route
     response = requests.post(
         ENDPOINT_APITOKEN,
-        # query parameter
         data={
         "grant_type" : "client_credentials",
-        # "client_id" : CLIENT_ID,
-        # "client_secret" : CLIENT_SECRET
         },
         headers={
             "Authorization" : f"Basic {decodedstring}",
             "Content-Type" : "application/x-www-form-urlencoded",
-        }
-    )
-    return response
-
-def auth_code(): # DEPRECATED
-    # Request user authorization
-    response = requests.get(
-        "https://accounts.spotify.com/authorize",
-        {
-            "client_id" : CLIENT_ID,
-            "response_type" : "code",
-            "redirect_uri" : REDIRECT_URI,
-            # "state" : randomString(LENGTH),
-            # "scope" : "playlist-read-private",
-
-            # PKCE Extension
-            # "code_challenge_method" : "S256",
-            # "code_challenge" : base64.b64encode(hashlib.sha256(randomString(50).encode('utf-8')).digest())
-        },
-    )
-    return response
-
-def request_access_token(code): # DEPRECATED
-    # Turns out for auth code flow we need a web server to recieve 
-    # access tokens so we will go for client credentials instead
-    to_encode = f"{CLIENT_ID}:{CLIENT_SECRET}".encode()
-    b64encoded = base64.urlsafe_b64encode(to_encode)
-    decodedstring = b64encoded.decode()
-    # Exchange authorization code for Access Token
-    response = requests.post(
-        url="https://accounts.spotify.com/api/token",
-        data={
-            "grant_type" : "authorization_code",
-            "code" : code,
-            "redirect_uri" : REDIRECT_URI,
-        },
-        headers={
-            "Authorization" : f"Basic {decodedstring}",
-            "Content-Type" : "application/x-www-form-urlencoded"
         }
     )
     return response
@@ -104,22 +39,45 @@ def get_playlist_items(access_token, playlist_id):
 
     return response
 
+def response_status_codes(response_auth): # Only applies to Get Playlist Items
+    if response_auth.status_code != 200:
+        print(response_auth)
+        print(response_auth.json())
+        if response_auth.status_code == 401:
+            print("Access token expired, relaunch app and it should be good.")
+        elif response_auth.status_code == 403:
+            print("Bad OAuth request, like wrong consumer key/expired timestamp.")
+        elif response_auth.status_code == 429:
+            print("Exceeded rate limits. Try again in 30 minutes.")
+        return 0
+    else:
+        print(response_auth)
+        print("Authentication succeeded!")
+    return 1
 
-def main():
-    # --- STAGE 0 ---
+def main(): # Traffic control
 
-    # Client Credentials
+    # --------- STAGE 0: Authorization ---------
     response_auth = auth_client_credentials()
-    print(response_auth)
-    print(response_auth.json())
-    print(response_auth.json()["access_token"])
+
+    if not response_status_codes(response_auth):
+        return
+
+    # --------- STAGE 1: Access Spotify API ---------
+    # Gets playlist ID from URL and passes it to the endpoint with wanted fields
+    playlist_id = re.search('https://open.spotify.com/playlist/(.*)\?', PLAYLIST_LINK).group(1)
+    endpoint_playlist_fields = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks?fields={FIELDS}"
+
+    items = get_playlist_items(
+        response_auth.json()['access_token'], 
+        endpoint_playlist_fields,
+        )
     
-    if response_auth.status_code == 200:
-        items = get_playlist_items(response_auth.json()['access_token'], ENDPOINT_PLAYLIST_WITH_FIELDS)
-        pprint.pprint(items.json(), indent=4)
+    # --------- STAGE 2: Data processing and duplication check ---------
+    with open(f"{playlist_id}.json", "w") as f:
+        json.dump(items.json(), f, indent=2)
 
     return
 
 if __name__ == '__main__':
     main()
-
